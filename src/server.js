4744303449f1d9184df5bd738caa890ef12d8f1a
Megha Roy <megharoy064330@gmail.com>
2026-03-19 02:21:53 -0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+qux6Vi1wpRnELaoK
